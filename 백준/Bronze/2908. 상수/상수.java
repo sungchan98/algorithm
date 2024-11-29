843d3f8536e397c7144a3df92b845a77e1b8179e
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-11-29 13:25:05 +0900

    [Bronze II] Title: 상수, Time: 188 ms, Memory: 18372 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String a = sc.next();
+    String b = sc.next();
+
+    String a1 = "";
+    String b1 = "";
+
+    for (int i = 2; i >= 0; i--) {
+      a1 += a.charAt(i);
+      b1 += b.charAt(i);
+    }
+
+    int numA = Integer.parseInt(a1);
+    int numB = Integer.parseInt(b1);
+
+    if (numA >= numB) {
+      System.out.println(numA);
+    } else {
+      System.out.println(numB);
+    }
+
+  }
+}
