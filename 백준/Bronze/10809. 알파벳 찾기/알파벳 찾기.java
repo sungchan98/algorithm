7f1b8cf7f4278b8a3977ad9b9522dd08ae49160d
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-12-11 18:24:22 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 196 ms, Memory: 18308 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String args[]) {
+
+    Scanner sc = new Scanner(System.in);
+
+    String s = sc.nextLine();
+
+    int arr[] = new int[26];
+
+    for (int i = 0; i < 26; i++) {
+      arr[i] = -1;
+    }
+
+
+    for (int i = 0; i < s.length(); i++) {
+      int idx = s.charAt(i) - 'a';
+
+      if (arr[idx] == -1) {
+        arr[idx] = i;
+      }
+    }
+
+    for (int i = 0; i < 26; i++) {
+      System.out.print(arr[i] + " ");
+    }
+  }
+}
