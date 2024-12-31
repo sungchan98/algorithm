4babaf9a8d86623303c9a4fa8042a5ef9e2fc4b3
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2024-12-31 17:26:27 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 196 ms, Memory: 18520 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    String str = sc.next();
+    String str2 = "";
+
+    for (int i = str.length() - 1; i >= 0; i--) {
+      str2 += str.charAt(i);
+    }
+
+    if (str.equals(str2)) {
+      System.out.print(1);
+    } else {
+      System.out.print(0);
+    }
+
+  }
+}
