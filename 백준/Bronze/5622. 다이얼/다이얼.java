6c7b640bbd8484f64ffd80007a163236bd6c2203
sungchan98 <150873466+sungchan98@users.noreply.github.com>
2025-01-22 15:50:53 +0900

    [Bronze II] Title: 다이얼, Time: 172 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    String[] dial = {"ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
+    
+    Scanner sc = new Scanner(System.in);
+    String word = sc.nextLine(); 
+    sc.close();
+    
+    int totalTime = 0;
+
+    for (int i = 0; i < word.length(); i++) {
+        char a = word.charAt(i);
+        for (int j = 0; j < dial.length; j++) {
+            if (dial[j].indexOf(a) != -1) {
+                totalTime += j + 3; 
+                break;
+            }
+        }
+    }
+
+    System.out.println(totalTime);
+    
+  }
+}
